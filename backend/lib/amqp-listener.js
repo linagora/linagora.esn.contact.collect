340087640754c2dabc69dcd642d2c4987cc3b15f
Michael Bailly <mbailly@linagora.com>
2017-12-13 19:03:05 +0100

    #8 listen on the global pubsub
    The global pubsub is resilient to reconnections

--- backend/lib/amqp-listener.js
+++ backend/lib/amqp-listener.js
@@ -2,29 +2,22 @@ const EXCHANGE_NAME = require('./constants').EVENTS.EXCHANGE_NAME;
 
 module.exports = dependencies => {
   const amqpClientProvider = dependencies('amqpClientProvider');
+  const globalPubsub = dependencies('pubsub').global;
   const logger = dependencies('logger');
   const handler = require('./handler')(dependencies);
-  let amqpClient;
 
   return {
     start
   };
 
   function start() {
-    const amqpClientPromise = amqpClientProvider.getClient();
-
-    return amqpClientPromise
-      .then(client => {
-        amqpClient = client;
-
-        amqpClient.subscribe(EXCHANGE_NAME, messageHandler);
-      })
-      .catch(err => logger.error('ContactCollector - Cannot connect to MQ', err));
+    globalPubsub.topic(EXCHANGE_NAME).subscribe(messageHandler);
   }
 
   function messageHandler(jsonMessage, originalMessage) {
     return handler.handle(jsonMessage)
-      .then(() => amqpClient.ack(originalMessage))
+      .then(amqpClientProvider.getClient)
+      .then(amqpClient => amqpClient.ack(originalMessage))
       .catch(err => logger.error('Fail to process message', err));
   }
 };
