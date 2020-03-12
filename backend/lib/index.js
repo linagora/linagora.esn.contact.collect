2abe1ded234fc0c2482c0d84cd78d26730ff8d9d
Christophe HAMERLING <chamerling@linagora.com>
2020-03-12 15:18:38 +0100

    esn#2904 Use point to point messaging to ensure a contact is collected only once

--- backend/lib/index.js
+++ backend/lib/index.js
@@ -1,11 +1,11 @@
 module.exports = dependencies => {
   const collector = require('./collector')(dependencies);
   const handler = require('./handler')(dependencies);
-  const amqpListener = require('./amqp-listener')(dependencies);
+  const listener = require('./listener')(dependencies);
 
   return {
     collector,
     handler,
-    amqpListener
+    listener
   };
 };
